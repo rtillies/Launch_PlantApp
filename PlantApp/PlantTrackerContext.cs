6bfc28093b223b824fa0529a6808c71537999e1c
Richard T <richard@turing.edu>
2023-05-30 16:30:37 -0400

    Add project files.

---
+++ PlantApp/PlantTrackerContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PlantApp
+{
+    public class PlantTrackerContext : DbContext
+    {
+        public DbSet<Plant> Plants { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(
+                "Host=localhost;" +
+                "Username=postgres;" +
+                "Password=password123;" +
+                "Database=PlantTracker").UseSnakeCaseNamingConvention();
+        }
+    }
+}
